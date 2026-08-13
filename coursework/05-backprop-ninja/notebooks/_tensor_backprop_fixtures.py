473744eed6da5036050ed9b12bbc27ccbd732eb1
Gilles Major <major.gilles@gmail.com>
2026-08-13 21:31:55 +0200

    Add tensor backpropagation exercises notebook, including 154 progressive exercises focused on manual backpropagation.

---
+++ coursework/05-backprop-ninja/notebooks/_tensor_backprop_fixtures.py
@@ -0,0 +1,189 @@
+"""Opaque fixture runner for tensor_backprop_exercises.ipynb.
+
+This generated support module keeps private test fixtures out of the visible
+notebook cells. It is not lesson content and contains no student answers.
+"""
+
+from __future__ import annotations
+
+import ast
+import base64
+from functools import lru_cache
+import hashlib
+import json
+import zlib
+
+_PAYLOAD_SHA256 = "798df4db65512d419be69c6d60d26905d990b8bca4ec0f42ed52776f4416ece4"
+_PAYLOAD = (
+    "c-"
+    "rk<YjfK+v;Hfb^o#9Ss(91Iot$aX%bU#U>DxK&)NvG#hLUJYClaZblwvuZ{`Z5$g#bu`1SL|Jw)qf?BnW`}>@Ic}3*e6zosRk9=E"
+    "YaWubb6sG54p&!+g8mME;EtZ31IGo5#l3e+VPrc#7uhdGLtt`Nq-<Hr~SUg80dg+8=|h@yb|-(PY+M`#~H=7goD-ZA{nCEC1E^zu"
+    "o=v!{x_d6r&RU2-"
+    "WrEMbnFqFZ?ZP{NsyjV{5!L>~;s0`O0_~#4CTYHo{=>jGKyC$L7Imvo_+HxANm}j7hjemp(2M`TyDY@fwv0H)|;L9B!i6Up%0mt)"
+    "h9bMvcA0Z_|%MZ+O$^_Qw~f$rsm%238X^7@9zY7AA!a|2gf>y%tq0LX|7SYP(0IirpMliiz@^d8vip>a<<{svaqA#$eA!B&5>_Au"
+    "%BcVf2WI>FqM6)T5unSn-"
+    "(jb8Dra7kR;>f6<k{za9Cw^A{H4E)_Lg#!Y7DsQRkg1VPMeqgPCmEK3ckTvAR%JT;U>TaU}Sr@&>s;}A8MAU#ZwcDz|$Tpp1m{Sz"
+    "WdpOa)dU%u)blP-f3qRT+0i+M!4n4Qz1i`hxh<=Xh<`z!eQTeR_yNhR|{hBK2`-$_v$8CS=oi`5ACSOtWE4_45|l4*k&3H}aYh-"
+    ")2H#_F)j7yiRqn^1^K4r)A_Kh82Q^%d_p1(d}mIqt!?2Cnpo#H_<IUwh+)pP{C!po~l8aXyW+#uP#Tn^1^)CbyhOp+y)xswj$$bl"
+    "i<0s`vhg^mH5Xbchd)y}yj(ca3{JN8}wnM&6VtcVeo7p^j|OkHHjuPNvdM+hXoht~WXHR?v4!Vr=<8UmJgXd@<dAd~suJ|3q(~dM"
+    "`O?wit%ZYLTdm%WLD~i)ViOS-1@^^CxgcA2M2Yn+{4~f;trl+CqdvM*3DkQSR9@cinQ!=-"
+    "NFzU3*P?<8Bp_Zk6oGVjV3F_Uvhp@kgRF&__&-"
+    "%_?3a*<DgKaFa($B?0>l3V92Ew^>=#udb{jt}N0ZS!(tVNzK7Ysm!Y@Tdgl$#^F{u4F?CLq19=GhFk=lY9wr#g}kWAhF0g0Bs3c$"
+    "p@qeH2O1(O6{$wT;7C!FQPDgk6`PqzXz?gp=7Wo<*!H)pi%SI+rGdJXiq;{iXg4g*RR-uSvb-"
+    "OaMC68!mPFf!q@vUKFwH1hftYJAm?^25H6Tm*(m5nW-KM15rP5uEmht>?8P2DA5?*SGTkauA*^G13vPB!R>Y#&s^u<PpIW0}*>>i"
+    "S$z2;|YwMhA;$QE@fy6;~KO#n?>)XI8C*irlfKfh!ldA(Q|G{9nz24iF9ef9|~Sx^`P&G<z&8TNB$ZM7jF?>o7CTL?i*34!FUl|s"
+    "-z3<QIwef!;|z$uu0FNEN#L<)2b;lV*5usaPxU{|r;zSQN=8DA?A5FEO4J!E$dgTSe|a+cDQt7PlhhxFuZ>j2SLEh5GfPnceiC}%"
+    "URu2<1){-DqyrjWp)ua-"
+    "#9UK+UKS_H((7ZBVKT;d<(7H$)6!H;~|VKOtPi1`bD=?CkldF&hLdsZ_oX|j)#8(3`lLtaQe#jUnmLS*~kA$PmM1URSq*i2}+^u0"
+    "hu#8zJZs!T=a@Kkggw(zS_@yUm|+qlRmajHhFE<ml&iZ6?}+{A-XAwX`vE}^x<4f->S8=m8g^T8uCqiS_=l+?VoMA6ZOj-"
+    "o(HxmQNRdS~YtiFzkRq9Q$Dm#TqYBNXbN921e0JFO|UvO&Ioc+ELDNqU;4fx9wN4h~LAr*oR5Ol9{e2s<P;C=T1-"
+    "mk`(K9G<x5K{Mc-"
+    "a=loe*gNLo$$8j>LYya8PEK7`rRnrESOjKODaTreC$W8iT#$;U{RM;^O035c!^g3k7}lL;+@K+g048)whsSpzoyGvAA@qm$lAL<I"
+    "q{TdlSKh?GV7@vcY`peY@vEKGDkZS4ZlyDWrE_=A;nNeHZiA@~cH)y(nyqLry{$SfQew(GjW|0_Gt+WIGx4t}nXug3QW7Eyxea@X"
+    "e!#QL-6|@95S3_y_GfRg@#9*|f!^Uc_Efy8Nx0Z7gE-"
+    "?`0lDe3;^<WQ_$B&Sv`U~c{fVhnk?2V+>>=x^mJ|$5!V(+Ly|`p5xq5~<RGPwx4;m(sJgaojpo=V0Fe;|(aFgf?{%QkAUSdz+N(("
+    "x-"
+    "bGpzCiJln>OHc1l1v|HM0@xjXrCAPix${nG6;=XN(G*j5;9)rvRItnZCf8I<JC)2aDZrwyu}An)4=$^r0m`&1N?ZP%<9?cw&KD}h"
+    "jXHb19mP#+>QEm;@>+r+T<T$HpEibjs?3H#v`+*?r|vbD>8OsC*z0=Kmlt1(U`i6{R6!GW@FiJ-+0-"
+    "B?`v1@#fCzsvtfXah2mi>zN?u?X!Igj>eVIMQmwMp3O@pfr*&Q0)t7g%4PY~Tk=3gzRa1nV0i#1pO)^4-"
+    "f4A9b!XE5!afZ5t>HiXyFrp{@YMl2S%2dcdj8Cd&&y*R)s@+HXxNYTCG`G>O13s(%_pxOW<^hXHXG$fU{E%pq5h7PQQzoz5DpiSh"
+    "HVY^@Mm^;t{w;AEw3E*z$R3}vciPUs;W`32eWPl=NI@Z$bNgC3w812FBAl%^ZE}>TPK6?UJdLWr6?t03;k5kR_{JO;R+|QKlnx`>"
+    "9cC7}di_+P~Lq2cp2$RRQIDJGG_vm>s;nN5WdL?-"
+    "|3{6_PDLln+5Nis25%O~vsZ44~oDy<F4_Es{=AItB#bWGDK9!DI3Xqpx{3**`@bKFB(Obmm4_p8r(M$2}y|jts)67SBL5<fSZmes"
+    "UO~rY;itrL^81As&+HApJW>482J;rcOAw;iX5NoLb8W!QkFpF$hc;g^$AQ!}*>Vw-kzAM|UBiN*z`>LGdQ-"
+    "=oew<|B0qHlSHDcC00;uf(<3H4S{`-3aSo>+r`D*K&*x=`mA!po~?1=t-"
+    "u9_Sw1cs`~doZXmd2b(*8jPIV4kKFAYWF47qzSL7b?!s!SYKxS=HJ*0Nwd^WlQ%`t_jy7UX{PvR`Aa%r|Uf1C+ZEFoDzD8JV>qyb"
+    "+iZQ6yfX3@W9r~J+`n7;S<&?SZ;8d30-"
+    "YQ02ldmF3a_o|wNA4?0&27mNkW!EoDMGjK^Hy7WZu#w7Es%PhhNP#eIr2v3J6b$mUU^`=qjK~{HFOCAN}A}ayPX}mdN^8imyxVD@"
+    "bitfWWC|H-w3zw`0cwCXmv!0-Zv4(Yf#bv%lA2wmNG-gYRF<R*{;Sin5F>cdqD7gp#;9?@VqaVzydJq2o-"
+    "%$foZn&s8QLf)W+;X$z12Yuvr3Z9{{Tl8s_2$4#x*wbFlzI`)??Z3iiO8ivpU}gt}oSv5RC|gI}r%`>t%{G{i>S041~=sE5(N2p2"
+    "8FI36v+38Ks^<8H4tWIY^y;uR7mx&vOh!<<tiKjrWS+=j-"
+    "56q}?gDfV&yVV`E$XTNFUo_j6~)cuEnnqZ)dJHA*@sfEgzsCzC<)T@<=cAN;oh_rtzj#`Tcow0PjBurD{ulITxi(+5;maTj1^-"
+    "m?9@};&w7vQuV-dr0$hk<_&TO<zQ*R^qT-_VS5uP*?nv3Z{&(DH@f{-"
+    "S5x4QdUv><}bD_bUWn7@~chdToUZd6(2XUM??QUmI^O%Pf3HBNAif+W3Rx1Y`Q0137=YWVzm|<!9%(fPNrGn|0Dy_B+Rk#q?_lte"
+    "F}_uR5T;x3ZNAn``V=Y17d<liEb6^XdrsP}~!Fn4GCmGXD0ZXq|~!#3Y80g)gjPhA{<>F$LauZwU{qy?DtwkTtzWP$;wDt5e2GcD"
+    "Yih;(M1^y?v|1>Q&*pmkHaq|3R+I?JbASJ_>Y=hS2uV-?wHibbYAQ*?E5JEyK=4Em(O*Tu*O$t7xo$3k_KHYHvr--"
+    "fw{s-e#OwtkZ9aPQS%E{npYR`QP%}Z}qhG$((rTp-PhT`Cxk4LnElAJpQm!DnMV%w(@|c-"
+    "YzM$9>48r%`*49r?tIU{GW{%;nX*B;2e;d-"
+    "rFly+!s@sXoWcIoc!E{)=4c%5<2Ur4&8gFa*mb1U5&QlSynW`aJ28<w*8)*Ksegc?7`7?7jn}YgapBnUbaiWe<FunsrGX&*`t3EK"
+    "2m__Iazp{Gjg}Cy`1}ECK9e1uQY}qyI@6Y;5NadE>?rnI#Npona+`yGwC=k4BAW+u=4$<`eIhn4A7bF0@RG(o@oIz=$!8PFG7B$r"
+    "JYh6PB)V^KLv)5KlD+RQK(!e4m#z-5N((w+v1<;gj-caG<lP!&f*(&4O#>9R7^3_>%>YxX0`7{Y4eO0dTEfyb(m=bGlL(84`V~y0"
+    "_}-^6(@8G=g*8IFPO}#978i`_AE@d`l=LWLa;?C+=kOwj#BL~y@92_HAa&d*r#<K`&B)e$%96==N&kI%XJ#!m_k--NZ}l&f%gjP4"
+    "03Z}lp{>7OF0TwYv{#>2JWe>tB%5_ICFvk#}6J?F$jpoS%{lkZV*7LbZKq8<GR{dT2!mIvTo2la>Py*7rj*n>};)UaeW0qvrU8py"
+    "xa;ZK+Zgw(XB0V59qCH$<_eJIcB*_wrWg7DG>P_mx3`=y$L9McXq*b9-8cMBog(sa>JfgR-"
+    "^P799G+eR%|>0ho{3azjfMO+TcEEO{R$%&F(>keH*uF4)*3XcHXWu;LP^)VS0H(jo^dUz;@O6tHMarZ#wKomBo&+w{^J25J~YjvY"
+    "sD3C^{&;JjOE+V?4M%pg;bB5!pXPM=NEINT0Qan!!nJb-"
+    "(ns^CjJ9gKl___n0MdY1rq66wtD?b&jnc;AjqQgKlv{n}A(z$j_ZNx?!~>d)<&5>CJSdhyb}EA!K$s2ih^N>hv9TJ>?@w68|$Xs`"
+    "Y%(@~Ac<L`{CDDs<Y!rj2H&j>yg08{<ufE@U&?RUihOe`$Pen4LV_*5M~V7|lVVkUi}$$AASiFl0~eYj9-"
+    "IhXYMe^;GRa3s~*Jk1Qzkd7{DKQk;WWj)fVB<yb#Q)a+QzA)qpLyM+kMr&&V;j2iS(LR2cPVeoh<gM?nbFPwN5m!l1(<?}eSL!E0"
+    "PZ&vBNG$LP}IHKP3hv=dlVuqr94k&_?Lvk^X03_dNBtI=J3$1+`&^paQYpJ2NszD3bngm!7R-"
+    "p6!P9X1)wXMhrMU*=Y0Nus_v`hs_dX;5!=u-"
+    "6D14C!!?5c=pb{f!i|LU^$u<!st_;LZ0?qw~|cC)zRFc36K(=UMhHw0)6oD1w6s8iU94PB+6?ETfDT!rBRdxDrSWe54~l!k|>2s9"
+    "=w)r}yjRS2)(>lBLpzZHu7=+K&B>>nA%!QWS}-tMz`f45-U8r)=+MK)D-"
+    "u0fCq3AjSMl<3_Xs?!gn(sx;1VVZSBiXdZ*9EF&JB3tlcCS#+ko+5E`gAZQx=&y4fC)lbtpZQ{c;ZH~mkaurLq`^i+p_SGWwlHy&"
+    "iM7bn^62r9G&hf2<sAGWloWID$UQM&OS&vtbohoC9;AH`;)C=~3U;#|*ylO040#+^2&?j4uBBP}4%<>WaaXCfW|MgN(S{f`>5ph4"
+    "2nE3;6CKbeenw|%zh!!2C#vEr;~nBqL>eOu7SH(mVn&U5utF?>2^N22Ou{9)^l=ejPCo{=LKs4!=Wr7t5<j##p6~G6l%nVjZ~EN+"
+    "_<|AMs-"
+    "N(w@T?`6p;bi<_HahD%R4CEBJ*`72^lzg^JhUAEr|p|hg$}%zic|y(ffdm9OAGOw3*1@e9hz!{bKX;xC?amYxFZ$B<E{c9OsgpC6"
+    "$PqU!xmB5$Fbg4p%7=tRpxD)sB?x;3N{ZBRfpOO@M0_+F_Gl%^--QOQczXq0llwCR}D=lgf&eA7ua}O^Bzr!gUIvI$}%-"
+    "2{TKIOew8mM{MX0^Q`;IZRV9`a`6FOS%4f45ZO7N9H~pFc|)IMp(&&{2Snacp5IFW<JJZYrC&zdB}Xz;Ik${gIhqHb6IkHJ)m6)^"
+    "5d~L90h$w$VB7oiD-|Lo1ecM4X7UQd{S-"
+    "po^AH`1Qznfh36_&TrlrXNt0)}D3Gl{M3Ztg9CXOHBw)5o?4}57HZ&IDxOK3G%gJdG1K1vzy<FwMPBO=9M$l!`|f-"
+    "IaE1pk??I5XnlTS*E0pGh8dTwdH29_r;=hmWj7<b(4PLu%s%n(y-"
+    "{umh#~4QW1iC4veSx;`jXg560$*RRL6%}1}5Yyph^XHKPseGI)k5#lYuvV*jDh#MS|=l9ArJ|!7D=<{>Ez=ZhhcdofvOyEkc-Jl5"
+    "+3jBGiqh1VM$)C50Kd+n!%>#`%^iEZWUL(y15wcM{YxL%=I)XUD`^$MSZ;ie6WOidbAP3j*x84#pAEVMx`)A*npkMQ;huq@H!i!_"
+    "{Xyqs6P;GI>Enz@>3=H&dh$l-(+>NDb618`KO-m5*#)l_k-"
+    "E7gR>p@b&qH37rU*cR$n$1TG;A~I4rxQy7C1rb1LT~BPa#Rq0vT6J?pIw(DIEpQny^moBaVSvx2X-"
+    "n*OhUGC_^<}o8%T^#ar^nFpS~gJ#S;V`B0gUGg5XF36$}@F;NXSgI2>=cBp5<^DmjuIy_O+Yj0-"
+    "1*nfxO243iSnxK6GM{1fRNYL!X%hyjgmF}=k5ab;xQX7msWOG7Vy8pD&8P>^a9_$nlS9QyktF-"
+    "_DFn8lEg65QZ^ybc4Ha3zmu8}#ZA@`t=5=@Fp=*HR&|M9$-^J(nSa>V-_<yn<38xj@P(m)YPt{iO--Bp=J}<P?=ku>yahzk+`Km5"
+    "K@@ISI1e2x>CN<8S?!A!i*^d{U~^tVNZ67Y69*#y1|Ka482D<8<d)q|h6OpFwqkI*r!3oMp@dWAwj2{P-"
+    "*FtaZH3A}xjB2qRV>D9yPKLSE?$?6dy@=j^}0J^L@{p8Xfp;q}f|kv|Ta*#3Tu&y}6SY82c?(dn{U=bM$7-"
+    "WGOZX{&U_3T_xLTfx7wRyQk=tT!QD6}FQn^$9!5lN+fKpnoP%)NuaY7#Z;o4+*Tu+G8|o<XB)tTY@XJnhz^kZNz}OVg0EeZBhg}B"
+    "9?&b6g$-"
+    "6PA7D3fE}P_*s_fLs^xgOeC!4sJBxtF=yyYJ=bR&0L}3nrl`dh)AlRMSK(G|?WJ_cFIHzFSU#Z{N$r}fs@h%t16YBh=Ri^Q^aq$L"
+    ";o>`Ofv^xZ-"
+    "&#;{ZhsokNSt_8EI1s2l!&Vu*0J=<X`1^(dX4V22Az)5)o*&b!r|1x@s3OyExwKm9Vv?^gUK*&trEztIQ{ZcY!AxM_(=(KYH{^aE"
+    "423jKYD*Q#PQcjKiK{J+L3ZwqqN{)SKoEIPiKM~7x(S_{(S^1}utm-"
+    "0YeG7VC1~C7F72cwOJOI)P=CZC=rY9>b}d{%sGUvMbLDBd>P+$I&4WC-"
+    "vuJD&!hE_cp*khlWD^IlI9#hPFfBa1?G)|d6@ebsZwKY4E$z-_I2nSr&iD*Ne8YGT--"
+    "14od+>|iOjc|M`D?Zdm<4Jeb@HchEiUUOos!KjZ<5L`x1D<9(hr-(!gvF5>7OC52w}9!jZm*=k?$L``E=?BM&NIvFo-eZBJz$>rg"
+    "<tXo!(9<2_z?TPo{929}S0>PF*LlZI*Xqlfs%T3*8GaoAHiH1_j)%S4v2Y^~nmUQ5<f0E6j0`KCi%&Wbe^INP@3rNVUp^t)#SI6t"
+    "f%5C=-eonq~|ZVxS4Na1(-8{L)AWAea-Z1RJG<&$=>FHHvJ?l#T#9FV7SZN07`b{Dx-"
+    "?n8*+?4Ri}+iv=ePFN)@$LE@57N8oSQM52sv<wxFn9tMfbOnGniRdJ-kaQuJ%WDOHZ2?1FX><d|&$ujXw<Q>mY;lOhy_MU1bHwG^"
+    "%PEO(LR2i;EIib^K5}lkNlP)0{0utUiB~zu+B?Mz$;NiSd=|br;f|o7Gl3a>A=%V}yae4R~`D;YF5oRAOIdZ(7t88V&KCAL*&_1t"
+    "-G|+ry{DOEHW+LOY_lQ_N@*n)j4<=|ZK!c01^42rpes66o{pis*7N{%?ER3H<bl=4hl+<CagubIG(tmgq6uC~Kbw1iS@;A(H<bKF"
+    "`>YmV%_q1{kQM+>w(IV4F&R99U*61JRQ`$c~);ASJ&S4(qj7eU9Q;}A?dxU-_LE_x#uuiXgXW8riS@wExj=gp|XW47BUJn74dB&z"
+    "pYclg9G*0-D@jpNPVjznS8A5a72P_w>5rpg<`tzrMfA`-"
+    "cEetMyAbXs^PdFCBB|m>hKfC0oY}|fj{0ROBED~Axuu3FCBO&yZc_iPU0cquXkhk>i`xtfplMl<l{OJ@*d6UUzxmjRy%LvDaa-"
+    "Z=sZPL{HvC2>Butj*x<-"
+    "kkE&E(p~&jn8fCsvt~Ga_ZHKy!CvMUo4u%FTZb2=G1n?g(;*;`Mxj#@onS!X5PGLCEQ`S>be{5PfhI0~=s?gK+JS!|)UR;K!r`|M"
+    "CN{LQx+s+XV1Y?EBLT%XE59-|jj++Uf83V#f$EWN>kT@wzrJn8=EFWHr6QEy(66GB-Qz+{J@X7@szht(<Vo6={Jq^j51OTh-7w(-"
+    "5vyr%qE@Cvz0RNJp-M^ciNAZWlB`J;#d%D#r)0WQqTlRQ|hG<(9JNRvy8B-?_s$ql-"
+    "&5Qj9eG;5|Id128(Dv97`j1h0$5ygMXPexE#;;03buBJrIt(~<9%c#nQe64DSV`PQHe{L&x{mdZNgYTcVekO~Ew6^Bxo#I~pg!#k"
+    "O_8Q{!`=j|4`@Z=rpzzbb%8A5GhF^iTH9Cva_+;*X0q0>%OJaO5R_nE`aD<rg$opth*xavapuMDXkPI%%L3j)VV5(k}&34KK%1o$"
+    "y*e^CJE-"
+    "(*c8<`v9Cz9L5uBi2IWZ4|DaDKts)9%+T*Z?P^|La8K2kiS=+BnV~YEJ2~wmXWHQlur@`Q!lejL0*QY3i2BTxq|Xr6jk{q$rglrE"
+    "CW#J&Xtj^A)$^~UP>7x&q>apSe7OYVvf5cGI}>I%oxP2r(HHyeQCO&*tQ~HFzI_)!eEvTMHz!~X;sP~FIAW`nBs`Sq`?$#sj>z+K"
+    "c+$$?BPtlPfo)kVv-7tBu~axHXP!S;~oSlzE%X5xK>bwmAHCP1eFq9QV>#-"
+    "G?OBrl$241lLXzW3MTO)%21M^*HwWeJ`Ttw$jHS?kgF78W`dZR>Ot~kY-VZ|s+6C7$DVwr7tn%u5@uXv;Sy12@zsDyEZ)0eM2{gz"
+    "H;W&lugW~<U&=CD<RTKelH3-#j7)VlYnUp+8Gud!r3-"
+    "LQOJ;epUEwT`3Z)(=4~QtrPbKk`){jZWGFpbNQIboEDvmF@$TXNP7H5iJt@~$tzV?^##pMl+fss81hyj*S+PQ2Xggn9NR0cbV(N~"
+    "n4%=*=Mm@{`Q;+j7d;LPZD<p9DWh;scnc1VY*Ol-"
+    "4C_wwVf8TFK>7z=u)6Ll@=Q6}D+7Q0Lz3;QERS=9x}*a%w?V1EQ50rUk^x3md1dfrQWV_mkVNC8feVcMP`N8i6FY@RBtNx0Z7gX&"
+    "#i0|W0uN6`fwF={;LT@qbdy%^S#KOr5^YAT^UuUBOOxZ<8VXVX*nY<k)~o1XU0rl<Y0>FMBHdg^x0rl;oF^wc_=o}SGZ<u)}&vFR"
+    "Ez+S512-sBTLSZ;-Sr`N|n{~x)L7`O"
+)
+
+
+@lru_cache(maxsize=1)
+def _sources() -> dict[str, str]:
+    raw = zlib.decompress(base64.b85decode(_PAYLOAD.encode("ascii")))
+    if hashlib.sha256(raw).hexdigest() != _PAYLOAD_SHA256:
+        raise RuntimeError("Tensor-backprop fixture payload failed its integrity check.")
+    sources = json.loads(raw.decode("utf-8"))
+    if len(sources) != 154:
+        raise RuntimeError("Tensor-backprop fixture payload is incomplete.")
+    return sources
+
+
+def _source_for(number: int) -> tuple[str, str]:
+    key = f"{number:03d}"
+    try:
+        return key, _sources()[key]
+    except KeyError as error:
+        raise ValueError(f"Unknown tensor-backprop exercise: {number}") from error
+
+
+def run_fixture(number: int, namespace: dict) -> None:
+    """Execute one fully opaque fixture in the notebook's global namespace."""
+    key, source = _source_for(number)
+    exec(compile(source, f"<tensor-backprop-fixture-{key}>", "exec"), namespace, namespace)
+
+
+def run_forward_reference(number: int, namespace: dict) -> None:
+    """Register a private reference while using inputs already visible in the notebook."""
+    if not 1 <= number <= 14:
+        raise ValueError("Visible forward references exist only for Exercises 001-014.")
+
+    key, source = _source_for(number)
+    tree = ast.parse(source, filename=f"<tensor-backprop-fixture-{key}>")
+    reference_calls = [
+        node
+        for node in tree.body
+        if isinstance(node, ast.Expr)
+        and isinstance(node.value, ast.Call)
+        and isinstance(node.value.func, ast.Name)
+        and node.value.func.id == "_store_forward"
+    ]
+    if len(reference_calls) != 1:
+        raise RuntimeError(f"Exercise {key} does not contain exactly one forward reference.")
+
+    reference_module = ast.fix_missing_locations(
+        ast.Module(body=reference_calls, type_ignores=[])
+    )
+    exec(
+        compile(reference_module, f"<tensor-backprop-reference-{key}>", "exec"),
+        namespace,
+        namespace,
+    )
